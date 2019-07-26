fe41c637d348d778d56013e1a195686bc547ddda
rouxcaesar <jrzerwick@gmail.com>
2019-07-26 09:58:44 -0400

    Finished maps section

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -43,6 +43,42 @@ func TestAdd(t *testing.T) {
   })
 }
 
+func TestUpdate(t *testing.T) {
+  t.Run("exisiting word", func(t *testing.T) {
+    word := "test"
+    definition := "this is just a test"
+    newDefinition := "new definition"
+    dictionary := Dictionary{word: definition}
+
+    err := dictionary.Update(word, newDefinition)
+
+    assertError(t, err, nil)
+    assertDefinition(t, dictionary, word, newDefinition)
+  })
+
+  t.Run("new word", func(t *testing.T) {
+    word := "test"
+    definition := "this is just a test"
+    dictionary := Dictionary{}
+
+    err := dictionary.Update(word, definition)
+
+    assertError(t, err, ErrWordDoesNotExist)
+  })
+}
+
+func TestDelete(t *testing.T) {
+  word := "test"
+  dictionary := Dictionary{word: "test definition"}
+
+  dictionary.Delete(word)
+
+  _, err := dictionary.Search(word)
+  if err != ErrNotFound {
+    t.Errorf("Expected %q to be deleted", word)
+  }
+}
+
 func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
   t.Helper()
 
