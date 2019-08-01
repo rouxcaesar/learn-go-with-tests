4376af0f6715bf3f979f458b74b83b8f48cbd74c
rouxcaesar <jrzerwick@gmail.com>
2019-08-01 14:16:20 -0400

    Ran go fmt

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-  "io"
-  "fmt"
-  "os"
-  "time"
+	"fmt"
+	"io"
+	"os"
+	"time"
 )
 
 func main() {
-  sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
-  Countdown(os.Stdout, sleeper)
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
 }
 
 const finalWord = "Go!"
 const countdownStart = 3
 
 type Sleeper interface {
-  Sleep()
+	Sleep()
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
-  for i := countdownStart; i > 0; i-- {
-    sleeper.Sleep()
-    fmt.Fprintln(out, i)
-  }
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
 
-  sleeper.Sleep()
-  fmt.Fprintln(out, finalWord)
+	sleeper.Sleep()
+	fmt.Fprintln(out, finalWord)
 }
